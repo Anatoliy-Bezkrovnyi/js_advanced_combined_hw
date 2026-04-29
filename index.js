c06b69fa1a37c00b5ca31e2fabe7db60653f26bf
Anatoliy-Bezkrovnyi <51107235+Anatoliy-Bezkrovnyi@users.noreply.github.com>
2026-04-29 23:20:29 +0000

    Deploying to gh-pages from @ Anatoliy-Bezkrovnyi/js_advanced_combined_hw@bd24fea2b5d28d266fcd08c558ab79adc44b1a6a 🚀

--- index.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-Bwb45YF3.js";const o={method:"GET"};fetch("https://jsonplaceholder.typicode.com/users",o).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}).then(t=>{}).catch(t=>{});
-//# sourceMappingURL=index.js.map
